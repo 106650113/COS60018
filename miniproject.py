9396b7dbe1b1045cbf0fd79376d4e59941dcded6
Nath <106650113@student.swin.edu>
2026-09-03 00:13:05 +1000

    create mini project and made functions and starting menu

---
+++ miniproject.py
@@ -0,0 +1,34 @@
+
+def user_guide():
+    menu_sel = input('Please select one of the following options')
+
+
+
+
+
+
+
+
+def SR_reg():
+    pass
+
+def search():
+    pass
+
+def add_on():
+    pass
+
+
+
+#Create a menu for the entire program
+
+#Create the following menu 
+#
+print('====================================')
+print('          Specimen Menu               ') 
+#print('  Please Select the following options')
+print('=====================================')
+print('1. Specimen Registration')
+print('2. Search')
+print('3. Add-on tests')
+user_guide()
