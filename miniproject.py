2394a68fb2c140b51701ea5ef52f45245ab0bf8e
Nath <106650113@student.swin.edu>
2026-09-03 12:06:49 +1000

    finalised code for user_guide, started SR_reg

--- miniproject.py
+++ miniproject.py
@@ -1,6 +1,24 @@
 
-def user_guide():
+def user_guide():#Function for navigating the user to the menu option that they have chosen
     menu_sel = input('Please select one of the following options')
+    if menu_sel == '1':
+        SR_reg()
+    elif menu_sel == '2':
+        search()
+    elif menu_sel == '3':
+        add_on()
+    else:
+        print('invalid selection')
+        while menu_sel !='1' and menu_sel !='2' and menu_sel !='3':
+                menu_sel = input('Please select from options 1,2 or 3 ')
+        if menu_sel == '1':
+            SR_reg()
+        elif menu_sel == '2':
+            search()
+        elif menu_sel == '3':
+            add_on()
+
+        
 
 
 
@@ -10,7 +28,10 @@ def user_guide():
 
 
 def SR_reg():
-    pass
+    print("Please enter the following patient details")
+    input('Full Name: ')
+    input('Date of Birth: ')
+    input("UR: ")
 
 def search():
     pass
