cbfcae20879b304e1a248f5fba1aecc7ff866968
Nath <106650113@student.swin.edu>
2026-09-06 03:18:39 +1000

    created Test Registration code

--- miniproject.py
+++ miniproject.py
@@ -1,4 +1,7 @@
 
+counter = 1
+
+
 def user_guide():#Function for navigating the user to the menu option that they have chosen
     menu_sel = input('Please select one of the following options')
     if menu_sel == '1':
@@ -21,17 +24,26 @@ def user_guide():#Function for navigating the user to the menu option that they 
         
 
 
+def SR_reg():#Purpose of this function is to create patient profiles
+    while True:
+        print("Please enter the following patient details")
+        pt_name = input('Full Name: ')
+        pt_dob = input('Date of Birth: ')
+        pt_ur = input("UR: ")
+
+        pt_details={'name':pt_name, 
+                    'dob':pt_dob,
+                    'UR':pt_ur}
+        reset = input ('Press 1 to register another patient or 2 to save and return to the main menu: ')
+        print(pt_details)
+        if reset == '1':
+            continue
+        elif reset == '2':
+            break
+    menu()
 
 
 
-
-
-
-def SR_reg():
-    print("Please enter the following patient details")
-    input('Full Name: ')
-    input('Date of Birth: ')
-    input("UR: ")
 
 def search():
     pass
@@ -45,11 +57,14 @@ def add_on():
 
 #Create the following menu 
 #
-print('====================================')
-print('          Specimen Menu               ') 
-#print('  Please Select the following options')
-print('=====================================')
-print('1. Specimen Registration')
-print('2. Search')
-print('3. Add-on tests')
-user_guide()
+def menu():
+    print('====================================')
+    print('          Specimen Menu               ') 
+    #print('  Please Select the following options')
+    print('=====================================')
+    print('1. Specimen Registration')
+    print('2. Search')
+    print('3. Add-on tests')
+    user_guide()
+
+menu()
