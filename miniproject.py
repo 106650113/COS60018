a9579fb989a7493ce813ff084638dc96031c8f2a
Nath <106650113@student.swin.edu>
2026-09-08 02:58:03 +1000

    added documentation for the code

--- miniproject.py
+++ miniproject.py
@@ -1,5 +1,5 @@
 
-pt_dict ={} #Creates and empty string to populate pt list
+pt_dict ={} # creates and empty list so that each patient that is created in the SR_reg function can be accessed outside of the function
 tests_dict = {}
 specimen_list_dict = {}
 
@@ -14,7 +14,7 @@ def user_guide():#Function for navigating the user to the menu option that they 
         search()
     elif menu_sel == '4':
         add_on()
-    else:
+    else: #if the user enters a number that is not present they will be prompted to choose a menu item form 1-4. As there is a while loop this will continue until the user enters an acceptable option 
         print('invalid selection')
         while menu_sel !='1' and menu_sel !='2' and menu_sel !='3' and menu_sel !='4':
                 menu_sel = input('Please select from options 1,2,3,4 ')
@@ -29,14 +29,14 @@ def user_guide():#Function for navigating the user to the menu option that they 
 
 
 
-def SR_reg():#Purpose of this function is to create patient profiles
+def SR_reg():# This function will create patient profiles which will later be used to register tests under
     while True:
         print("Please enter the following patient details")
         pt_name = input('Full Name: ')
         pt_dob = input('Date of Birth: ')
-        pt_ur = input("UR: ")
+        pt_ur = input("UR: ") #UR number is a unique numerical patient identifier
         #Here I am creating a patient dictionary within a dictionary the first key would be the patient's unique medical record number (UR) and the secondary dictionary contains the patient's name and date of birth
-        pt_dict[pt_ur] = {'name':pt_name,'dob':pt_dob,}
+        pt_dict[pt_ur] = {'name':pt_name,'dob':pt_dob,} #Here I am adding the patient details to the patient dictionary. Which will create a dictionary within a dictionary. The first key of the first dictionary is the patient's ur number and within that dicionary is another dictionary that contains the patient's name and dob
         reset = input ('Press 1 to register another patient or 2 to save and return to the main menu: ')
         print(pt_dict)
         if reset == '1':
@@ -51,11 +51,29 @@ def test_reg():
         ur = input(" Please enter the patient's UR number: ")
         if ur in pt_dict:
                 specimen_num = int(input('How many specimens did you recieve? '))
+
                 specimens_list = []
+                tests_list = []
                 for specimens in range(specimen_num):
-                    spec_type = input('Please specify each specimen type ')
-                    new_specimen_list = specimens_list.append(spec_type)
-                reset = input('To register another patient enter 1 to return to the main menu press 2 ')
+                    spec_type = input(f'Please specify specimen {specimens + 1} ')
+                    spec_test = input(f'Please enter all tests requested on the {spec_type} tube ')
+                    check_1 = input ('Is the fube adequately filled? (Y or N)')
+                    check_2 = input ('Is the tube fully labelled? (Y or N)')
+                    if check_1 == 'N' and check_2 == 'N':
+                        print('Organise specimen recollection')
+                    elif check_1 == 'N' and check_2 == 'Y':
+                        print('Consult with senior scientist')
+                    elif check_1 == 'Y' and check_2 == 'N':
+                        print('Call nurse to label tube')
+                    else:
+                        tests_list.append(spec_test)
+                        tests_dict[ur] = tests_list
+                    
+
+                    specimens_list.append(spec_type)
+                specimen_list_dict[ur] = specimens_list
+
+                reset = input('To register tests for another patient please enter 1 to return to the main menu press 2 ')
                 if reset == '1':
                     continue
                 elif reset =='2':
@@ -69,11 +87,12 @@ def test_reg():
 def search():
     pass
 
-#Create a menu for the entire program
+def add_on():
+    pass
 
-#Create the following menu 
-#
-def menu():
+
+#Creates a menu so that the user can pick which function to invoke
+def menu(): 
     print('====================================')
     print('          Specimen Menu               ') 
     print('=====================================')
