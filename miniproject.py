fad067dd3ea9e863327dcbd122e616df310bb5e6
Nath <106650113@student.swin.edu>
2026-09-08 01:10:52 +1000

    create code to allow user to reg specimens and make questions to ensure that specimens can be processed

--- miniproject.py
+++ miniproject.py
@@ -1,7 +1,7 @@
 
-pt_list ={} #Creates and empty string to populate pt list
-specimen_list = {}
-tests_list = {}
+pt_dict ={} #Creates and empty string to populate pt list
+tests_dict = {}
+specimen_list_dict = {}
 
 
 def user_guide():#Function for navigating the user to the menu option that they have chosen
@@ -27,7 +27,6 @@ def user_guide():#Function for navigating the user to the menu option that they 
                 elif menu_sel == '4':
                     add_on()
 
-        
 
 
 def SR_reg():#Purpose of this function is to create patient profiles
@@ -36,11 +35,10 @@ def SR_reg():#Purpose of this function is to create patient profiles
         pt_name = input('Full Name: ')
         pt_dob = input('Date of Birth: ')
         pt_ur = input("UR: ")
-
-        pt_list[pt_ur] = {'name':pt_name, 
-                    'dob':pt_dob,}
+        #Here I am creating a patient dictionary within a dictionary the first key would be the patient's unique medical record number (UR) and the secondary dictionary contains the patient's name and date of birth
+        pt_dict[pt_ur] = {'name':pt_name,'dob':pt_dob,}
         reset = input ('Press 1 to register another patient or 2 to save and return to the main menu: ')
-        print(pt_list)
+        print(pt_dict)
         if reset == '1':
             continue
         elif reset == '2':
@@ -49,29 +47,27 @@ def SR_reg():#Purpose of this function is to create patient profiles
 
 
 def test_reg():
-    ur = input(" Please enter the patient's UR number: ")
-    for finder in pt_list:
-        if finder == ur:
-            specimen_num = input('How many specimens did you recieve? ')
-            for indiv_spec in specimen_num:
-                spec_type = input('Please specify each specimen type ')
-                specimen_list.append(spec_type)
-                print(specimen_list)
+    while True:
+        ur = input(" Please enter the patient's UR number: ")
+        if ur in pt_dict:
+                specimen_num = int(input('How many specimens did you recieve? '))
+                specimens_list = []
+                for specimens in range(specimen_num):
+                    spec_type = input('Please specify each specimen type ')
+                    new_specimen_list = specimens_list.append(spec_type)
+                reset = input('To register another patient enter 1 to return to the main menu press 2 ')
+                if reset == '1':
+                    continue
+                elif reset =='2':
+                    break
 
-
-
-
-
-
-
+        else:
+            print('Patient does not exist. Please complete patient registration')
+            SR_reg()
+    menu()
     
 def search():
     pass
-
-def add_on():
-    pass
-
-
 
 #Create a menu for the entire program
 
